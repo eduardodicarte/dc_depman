35054fb3e74bb817851d25c446299ed892ce49c5
Eduardo Dicarte <eduardo_dicarte@yahoo.com.br>
2016-08-26 00:34:18 -0300

    Criacao do link para referenciar o maven instalado dentro da VM para integrar com a maquina fisica do usuario. Tratamento para que seja realizado o download do maven uma unica vez

--- manifests/maven/install.pp
+++ manifests/maven/install.pp
@@ -1,21 +1,27 @@
 class dc_depman::maven::install {
-  archive { '/tmp/apache-maven-3.3.9-bin.tar.gz':
+  archive { '/opt/maven/apache-maven-3.3.9-bin.tar.gz':
     ensure        => present,
     extract       => true,
-    extract_path  => '/opt/',
+    extract_path  => '/opt/maven/',
     source        => 'https://archive.apache.org/dist/maven/maven-3/3.0.4/binaries/apache-maven-3.0.4-bin.tar.gz',
     checksum      => 'e513740978238cb9e4d482103751f6b7',
     checksum_type => 'md5',
-    cleanup       => false
+    cleanup       => false,
+    require 	  => File['/opt/maven/']
   }
 
   file_line { 'm2_home':
     path => '/etc/bashrc',
-    line => 'export M2_HOME=/opt/apache-maven-3.0.4'
+    line => 'export M2_HOME=/opt/maven/apache-maven-3.0.4'
   }
 
   file_line { 'm2_home_path':
     path  => '/etc/bashrc',
     line  => 'export PATH=$PATH:$M2_HOME/bin',
+  }
+
+  file { '/opt/maven':
+	ensure  => 'link',
+	target  => '/vagrant/maven',
   }
 }
